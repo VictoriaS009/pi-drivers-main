49e0d6259b1b47058e1a53f4869b2a835534267a
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-01-28 18:29:35 -0300

    (ryzen)(maqueta front)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
 
-import './App.css'
+import { Routes, Route, useLocation } from "react-router-dom";
+//import LandingPage from "./components/LandingPage/LandingPage";
+//import Cards from "./components/Cards/Cards";
+//import Nav from "./components/Nav/Nav";
+//import Detail from "./components/Detail/Detail";
+//import FormCreateDriver from "./components/FormCreateDriver/FormCreateDriver";
+//import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
 
   return (
-    <>
-      <div>
-       <h1>problemas críticos</h1> 
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      {location.pathname !== "/" && <NavBar />}
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Cards />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/form" element={<FormCreateDriver />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+<div className="App">
+{pathname !== "/login" && <Nav onSearch={onSearch} />}
+
